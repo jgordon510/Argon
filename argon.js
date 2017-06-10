44f90c7d0754e1152ce362281ab9da8660017a54
Jeffrey Gordon <jgordon.oakland@gmail.com>
2017-06-10 14:32:53 +0000

    top block inputs; comments

--- argon.js
+++ argon.js
@@ -20,72 +20,77 @@ var Argon = {
         });
     },
     renderBlocks: function(data) {
-
         console.log("rendering blocks from file");
-        if (data.objName === "Sprite1") { //for now
+        //for now, we'll render just sprite1
+        if (data.objName === "Sprite1") {
+            //the scriptObj holds the scripts once they've
+            //been organized to match the xml structure of blockly
             var scriptObj = {
                 block: []
             };
 
             data.scripts.forEach(function(script) {
-                var blocksDown = 0;
-                var blockArray = script[2].shift()
-                
-                var newBlock = {
-                    _type: blockArray[0],
-                    _id: makeid(),
-                    value: [],
-                    _x: script[0],
-                    _y: script[1],
-                }
-                scriptObj.block.push(newBlock);
-                addInputs(scriptObj, blockArray)
-                console.log(blockArray)
-                
-                addBlock(scriptObj.block[scriptObj.block.length - 1], blockArray);
+                //a blockArray to hold the new script blocks
+                var blockArray = script[2].shift() 
 
-                function addBlock(block, blockArray) {
-                    console.log(block, blockArray)
+                //the top block of the script
+                var newBlock = {
+                    _type: blockArray[0], //the block name
+                    _id: makeid(), //20 char random id
+                    value: [], //a place to store the input value (repeat loop or something)
+                    _x: script[0], //the xLoc
+                    _y: script[1], //the yLoc
+                };
+                //add the new block to the scriptObj block array
+                scriptObj.block.push(newBlock);
+                //add the inputs to the top block
+                addInputs({
+                    block: newBlock //could add the last block of scriptObj instead here
+                }, blockArray);
+
+                
+                addNextBlock(scriptObj.block[scriptObj.block.length - 1]);
+
+                function addNextBlock(targetBlock) {
+                    //still blocks left
                     if (script[2].length > 0) {
+                        //get the next block from the script
                         var blockArray = script[2].shift();
+                        //make a block object for the new block
                         var blockObj = {
                             _type: blockArray[0],
                             _id: makeid(),
                             value: [],
                             statement: []
                         };
-
-                        block.next = {
+                        
+                        //add the block to the next of the targetBlock
+                        targetBlock.next = {
                             block: blockObj
                         };
-                        addInputs(block.next, blockArray)
-                        console.log("adding block")
-                        console.log(block.next.block.statement.length)
-                        if(block.next.block.statement.length>0)
-                        {
-                            console.log(block)
-                            
-                            addBlock(block.next.block, blockArray);
-                            
-                        } else
-                        {
-                            addBlock(block.next.block, blockArray);
-                        }
-                        
+                        //add any inputs to the block
+                        addInputs(targetBlock.next, blockArray);
+                        //recursion until the blocks are used used up from the shift
+                        addNextBlock(targetBlock.next.block);
                     }
                 }
 
-                function addInputs(scriptObj, scriptData) {
-                    console.log(scriptObj, scriptData)
-                    for (var i = 1; i < scriptData.length; i++) {
-                        if (scriptData[i] != null) {
+                function addInputs(scriptObj, blockArray) {
+                    console.log(scriptObj, blockArray);
+                    //go through the blockArray starting with the 1st input
+                    //which is at index 1
+                    for (var i = 1; i < blockArray.length; i++) {
+                        //ignore null values
+                        if (blockArray[i] != null) {
                             //here we've found an array of new blocks nested in our existing block
                             //we'll need to add each of them, possibly calling this function again recusively
-                            if (typeof scriptData[i] === 'object') { //an array
-                                console.log("test")
-                                scriptData[i].reverse()
-                                scriptData[i].forEach(function(block) {
-                                    scriptObj.block.statement.push({
+                            if (typeof blockArray[i] === 'object') { //an array actually
+                                //for some reason they come reversed
+                                //remember this when you're working on saving
+                                blockArray[i].reverse();
+                                //go through the blocks adding each to the statement
+                                blockArray[i].forEach(function(block) {
+                                    var position = scriptObj.block.statement.push({
                                         _name: "VALUE" + i.toString(),
                                         block: {
                                             _type: block[0],
@@ -95,22 +100,20 @@ var Argon = {
                                             next: []
                                         }
                                     });
-                                    
-                                    console.log(JSON.parse(JSON.stringify(script[2])));
-                                    addInputs(scriptObj.block.statement[scriptObj.block.statement.length - 1], block)
-      
-                                });
-
-
+                                    //add any inputs here
+                                    //position is one more than the index of the statement 
+                                    addInputs(scriptObj.block.statement[position-1], block);
+                                }); //end of statement forEach
                             }
-                            else {
+                            else {  //just a regular input
+                                //push it onto the value stack
                                 scriptObj.block.value.push({
                                     _name: "VALUE" + i.toString(),
                                     block: {
                                         _type: "input",
                                         _id: makeid(),
                                         field: {
-                                            __text: scriptData[i].toString(),
+                                            __text: blockArray[i].toString(),
                                             _name: "FIELDNAME"
                                         }
                                     }
@@ -121,38 +124,47 @@ var Argon = {
                     }
                 }
             });
+            //make a new X2JS conversion object
             var x2js = new X2JS();
+            
+            //wrap the scriptObj in an xml tag (could be done above)
             var finalObject = {
                 xml: {
                     _xmlns: "http://www.w3.org/1999/xhtml",
                     block: scriptObj.block
                 }
             }
+            //make the xmlText for blockly
             var xmlText = x2js.json2xml_str(finalObject)
             console.log(xmlText)
             if (xmlText) {
+                //clear the old space out
                 Blockly.mainWorkspace.clear();
+                //inject the blocks into the dom
                 Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xmlText), Blockly.mainWorkspace);
             }
         }
-
+        
+        //this makes a random 20-char id
         function makeid() {
             var text = "";
             var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
             for (var i = 0; i < 20; i++)
                 text += possible.charAt(Math.floor(Math.random() * possible.length));
-
             return text;
         }
 
     }
 };
 
+
+//test functions below
+
+//these save/load functions allow the creation of proper
+//xml for checking the output we've generated based on sb2 files
 window.saveWorkspace = function() {
     var xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
     var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
-
     localStorage.setItem("blockly.xml", xmlText);
 };
 
@@ -163,8 +175,8 @@ window.loadWorkspace = function() {
         Blockly.mainWorkspace.clear();
         xmlDom = Blockly.Xml.textToDom(xmlText);
         var x2js = new X2JS();
-
         console.log(x2js.xml_str2json(xmlText));
+        //this xmlText can be checked against what we're generating
         console.log(xmlText)
         var x2js = new X2JS();
         var jsonObj = x2js.xml_str2json(xmlText);
@@ -176,6 +188,5 @@ window.loadWorkspace = function() {
 function test() {
     var text = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="forward:" id="FOCtyk8mE8txmXECod4D" x="26" y="35"><value name="VALUE"><block type="input" id="iPA%_v|JlL0}14~o!.s~"><field name="FIELDNAME">10</field></block></value></block></xml>'
     var x2js = new X2JS();
-
     console.log(x2js.xml_str2json(text));
 }

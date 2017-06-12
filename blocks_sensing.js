78b0da8232899bd173f13899bc290e977780f9f2
Jeffrey Gordon <jgordon.oakland@gmail.com>
2017-06-12 01:23:32 +0000

    sensing menu

--- blocks_sensing.js
+++ blocks_sensing.js
@@ -82,7 +82,7 @@ Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
     "output": "Boolean"
   }, {
     "type": "distanceTo:",
-    "message0": 'distrance to %1',
+    "message0": 'distance to %1',
     "args0": [{
       "type": "field_dropdown",
       "name": "VALUE1",
@@ -93,25 +93,22 @@ Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
     }],
     "colour": 200,
     "output": null
-  },
-  {
+  }, {
     "type": "doAsk",
     "message0": "ask %1 and wait",
-    "colour":200,
+    "colour": 200,
     "nextStatement": null,
     "previousStatement": null,
     "args0": [{
       "type": "input_value",
       "name": "VALUE1"
     }]
-  },
-  {
+  }, {
     "type": "answer",
     "message0": "answer",
-    "colour":200,
+    "colour": 200,
     "output": null
-  },
-  {
+  }, {
     "type": "keyPressed:",
     "message0": 'key %1 pressed?',
     "args0": [{
@@ -164,6 +161,136 @@ Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
     }],
     "colour": 200,
     "output": "Boolean"
-  }
+  }, {
+    "type": "mousePressed",
+    "message0": "mouse down?",
+    "colour": 200,
+    "output": "Boolean"
+  }, {
+    "type": "mouseX",
+    "message0": "mouse x",
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "mouseY",
+    "message0": "mouse y",
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "soundLevel",
+    "message0": "loudness",
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "senseVideoMotion",
+    "message0": 'video %1 on %2',
+    "args0": [{
+      "type": "field_dropdown",
+      "name": "VALUE1",
+      "options": [
+        ["motion", "ITEM1"],
+        ["direction", "ITEM2"]
+      ]
+    }, {
+      "type": "field_dropdown",
+      "name": "VALUE2",
+      "options": [
+        ["stage", "ITEM1"],
+        ["this sprite", "ITEM2"]
+      ]
+    }],
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "setVideoState",
+    "message0": 'turn video %1',
+    "args0": [{
+      "type": "field_dropdown",
+      "name": "VALUE1",
+      "options": [
+        ["on", "ITEM1"],
+        ["off", "ITEM2"],
+        ["on-flipped", "ITEM3"]
+      ]
+    }],
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "setVideoTransparency",
+    "message0": "set video transparency to %1%",
+    "colour": 200,
+    "nextStatement": null,
+    "previousStatement": null,
+    "args0": [{
+      "type": "input_value",
+      "name": "VALUE1"
+    }]
+  }, {
+    "type": "timer",
+    "message0": "timer",
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "timerReset",
+    "message0": "reset timer",
+    "colour": 200,
+    "nextStatement": null,
+    "previousStatement": null
+  }, {
+    "type": "getAttribute:of:",
+    "message0": '%1 of %2',
+    "args0": [{
+      "type": "field_dropdown",
+      "name": "VALUE1",
+      "options": [
+        ["x position", "ITEM1"],
+        ["y position", "ITEM2"],
+        ["direction", "ITEM3"],
+        ["costume #", "ITEM4"],
+        ["costume name", "ITEM5"],
+        ["size", "ITEM6"],
+        ["volume", "ITEM7"]
+      ]
+    }, {
+      "type": "field_dropdown",
+      "name": "VALUE2",
+      "options": [
+        ["stage", "ITEM1"],
+        ["Sprite1", "ITEM2"]
+      ]
+    }],
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "timeAndDate",
+    "message0": 'current %1',
+    "args0": [{
+      "type": "field_dropdown",
+      "name": "VALUE1",
+      "options": [
+        ["year", "ITEM1"],
+        ["month", "ITEM2"],
+        ["date", "ITEM3"],
+        ["day of week", "ITEM4"],
+        ["hour", "ITEM5"],
+        ["minute", "ITEM6"],
+        ["second", "ITEM7"]
+      ]
+    }],
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "timestamp",
+    "message0": "days since 2000",
+    "colour": 200,
+    "output": null
+  }, {
+    "type": "getUserName",
+    "message0": "username",
+    "colour": 200,
+    "output": null
+  },
 
 ]); // END JSON EXTRACT (Do not delete this comment.)
+
+
